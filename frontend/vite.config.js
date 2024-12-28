b114b3a3958048e904868523ac0110f200fee669
Nitin Goley <113915570+nitingoley@users.noreply.github.com>
2024-12-28 19:06:05 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,16 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'public', // Change 'dist' to 'public'
-  },
   server: {
     proxy: {
       "/api": {
-        target: "https://e-commerce-1-6q5w.onrender.com",  
-        changeOrigin: true,  
+        target: "http://localhost:7000"
       }
     }
   }
-});
+})
 
